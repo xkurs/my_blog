57939668197b71f7e909b6a312ace134711c57a6
xkurs <xkurs@yandex.ru>
2022-10-18 20:45:26 +0300

    6

--- App.py
+++ App.py
@@ -6,9 +6,17 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+@app.route("/secret/")
+def secret():
+    my_dict = {"alex": "Hi!", "ivan": "Hello!"}
+    return render_template('index.html')
+
+
 @app.route("/about/")
 def about():
     return render_template('about.html')
+
 
 @app.route("/blog/")
 def blog():
