0a5f69a45af2a07d921e6b400f236f8f58e0a9c8
xkurs <xkurs@yandex.ru>
2022-10-24 20:22:12 +0300

    11

--- App.py
+++ App.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 
-@app.route("/")
 @app.route("/index/")
+@app.route("/")
 def index():
     return render_template('index.html')
 
@@ -11,8 +11,7 @@ def index():
 @app.route("/secret/<name>/")
 def secret(name):
     my_dict = {"alex": "Hi, Alex!", "ivan": "Hello, Ivan!"}
-    result = my_dict.get(name, "Привет, незнакомец!")
-    return f'<h1>{result}</h1> <p>Тебе сюда: </p> <a href="../index">Главная</a>'
+    return f'<h1>{my_dict.get(name, "Привет, незнакомец!")}</h1> <p>Тебе сюда:</p> <a href={url_for("index")}>Главная</a>'
 
 
 @app.route("/about/")
